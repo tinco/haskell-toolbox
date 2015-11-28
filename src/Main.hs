bc2054ae2444bc703e17c5c968a1a9d4ac6896ef
Tinco Andringa <mail@tinco.nl>
2015-11-28 19:15:42 +0100

    calculate package dependants correctly

--- src/Main.hs
+++ src/Main.hs
@@ -3,10 +3,14 @@ module Main where
 
 import qualified Distribution.Hackage.DB as DB
 import qualified Distribution.PackageDescription as Package
+import qualified Distribution.PackageDescription.Configuration as PackageConfiguration
+import qualified Distribution.Package as Package (packageName)
 import qualified Data.Map as Map
 import qualified Data.List as List
 import qualified Data.Set as Set
 import qualified Data.Text as Text
+
+import Debug.Trace
 
 main :: IO ()
 main = do
@@ -16,18 +20,29 @@ main = do
 	--putStrLn "Calculating categories.."
 	--mapM_ putStrLn (getCategories db)
 	--putStrLn "Done."
+
+	let packageDescriptions = buildPackageDescriptions db
+
 	putStrLn "Calculating dependency scores.."
-	let dependantsCounts = buildDependantsCounts db
+	let dependantsCounts = buildDependantsCounts packageDescriptions
+	putStrLn $ "Got dependantsCounts: " ++ (show dependantsCounts)
 	let dependantStrings = Map.mapWithKey (\ n v -> n ++ " " ++ (show v)) dependantsCounts
 	mapM_ putStrLn dependantStrings
 	putStrLn "Done."
 
-buildDependantsCounts :: DB.Hackage -> Map.Map String Int
+type PackageDescriptions = Map.Map String Package.PackageDescription
+
+buildPackageDescriptions :: DB.Hackage -> PackageDescriptions 
+buildPackageDescriptions db = foldl insert Map.empty $ map (resolve . List.last . Map.elems) $ Map.elems db 
+	where
+		insert m package = Map.insert (DB.unPackageName . Package.packageName $ package) package m
+		resolve = PackageConfiguration.flattenPackageDescription
+
+buildDependantsCounts :: PackageDescriptions -> Map.Map String Int
 buildDependantsCounts db = foldl insertDependant Map.empty dependencies
 	where
 		packages = Map.elems db
-		cabals = map (Package.packageDescription . List.last . Map.elems) packages
-		dependencies = map (\ (DB.Dependency n _) -> DB.unPackageName n) $ concatMap Package.buildDepends cabals
+		dependencies = map (\ (DB.Dependency n _) -> DB.unPackageName n) $ concatMap Package.buildDepends packages
 		insertDependant m d = Map.insertWith' (\ _ v -> v + 1) d 0 m
 
 getCategories :: DB.Hackage -> [String]
