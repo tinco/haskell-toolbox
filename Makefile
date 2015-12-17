8def53de810f2ddd374a92c4398fa8b1bdb25101
Tinco Andringa <mail@tinco.nl>
2015-12-17 01:56:17 +0100

    generate html list of categories

--- Makefile
+++ Makefile
@@ -2,4 +2,7 @@ all:
 	stack build
 
 run:
-	stack exec haskell-toolbox
+	stack exec haskell-toolbox build
+
+clean:
+	stack exec haskell-toolbox clean
--- src/Main.hs
+++ src/Main.hs
@@ -4,21 +4,49 @@ module Main where
 import Packages
 import qualified Data.Map as Map
 import qualified Data.List as List
+
+import           Data.Monoid     ((<>))
 import Hakyll
+import Data.String
+import Data.Ord
+
+data CachedOperations = CachedOperations {
+  categories :: !Categories,
+  packageDescriptions :: !PackageDescriptions,
+  packageScores :: !ScoreMap,
+  categoryScores :: !ScoreMap
+}
 
 main :: IO ()
 main = do
   db <- readHackage
-  generateMainPage
+  let packageDescriptions = buildPackageDescriptions db
+  let categories = getCategories db
+  let packageScores = buildDependantsCounts $ Map.elems packageDescriptions
+  let categoryScores = buildCategoryScores categories packageScores
+  
+  let cache = CachedOperations {
+	categories = categories,
+	packageDescriptions = packageDescriptions,
+	packageScores = packageScores,
+	categoryScores = categoryScores
+  }
+  generateMainPage cache
 
-generateMainPage = hakyll $ do
+scoreSort :: (a, Int) -> (a, Int) -> Ordering
+scoreSort a b = comparing (Down . snd) a b
+
+generateMainPage cache = hakyll $ do
   -- Read templates
   match "templates/*" $ compile templateCompiler
 
   match "index.html" $ do
 	route idRoute
 	compile $ do
-	  let indexContext = defaultContext
+	  let top20Categories = map (\ (n,i) -> Item (fromString n) (n,i) ) $ take 20 $ List.sortBy scoreSort $ Map.assocs $ categoryScores cache
+	  let indexContext =
+		listField "top20categories" (topCategoryContext cache) (return top20Categories) <>
+		defaultContext
 
 	  getResourceBody
 		>>= applyAsTemplate indexContext
@@ -26,6 +54,10 @@ generateMainPage = hakyll $ do
 		>>= loadAndApplyTemplate "templates/default.html" indexContext
 		>>= relativizeUrls
 
+topCategoryContext :: CachedOperations -> Context (String, Int)
+topCategoryContext cache =
+  field "name" (return.fst.itemBody) <>
+  field "score" (return.show.snd.itemBody)
 
 debugPackages :: IO ()
 debugPackages = do
