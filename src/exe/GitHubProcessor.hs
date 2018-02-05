bc5862164c2c98ed5f901fd9c4094bf6b9e51ecb
Tinco Andringa <mail@tinco.nl>
2018-02-05 18:54:06 +0100

    implement readGithubProjects

--- src/exe/GitHubProcessor.hs
+++ src/exe/GitHubProcessor.hs
@@ -5,6 +5,12 @@ import qualified Distribution.PackageDescription as Package
 import qualified Distribution.Hackage.DB as DB
 import qualified Data.Map as Map
 import HaskellToolbox.Packages
+import qualified Data.Vector as V
+import qualified Data.Aeson as AE
+import qualified Data.Maybe as M
+import qualified Data.ByteString.Lazy as B
+import qualified System.Directory as D
+import Control.Monad
 
 type PackageDescriptions = Map.Map String Package.PackageDescription
 
@@ -21,5 +27,10 @@ main = do
   return ()
   -- TODO annotate package descriptions with github info
 
-readGithubProjects = undefined
+readGithubProjects = do
+  filenames <- liftM (filter (\ f -> (take 5 f) == "repos")) $ D.listDirectory $ "data/"
+  files <- mapM B.readFile filenames
+  let parsedFiles = M.mapMaybe AE.decode' files
+  return $ AE.Array $ V.concatMap (\(AE.Array v) -> v) (V.fromList parsedFiles)
+
 filterOnCabal = undefined
