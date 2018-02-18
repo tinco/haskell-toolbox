a66164e85696d57e47c116a3c15c8e9ca1cd60f8
Tinco Andringa <mail@tinco.nl>
2018-02-18 01:12:53 +0100

    make github repo list simpler en filter on best starred github repos

--- src/exe/GitHubProcessor.hs
+++ src/exe/GitHubProcessor.hs
@@ -13,15 +13,30 @@ import qualified Data.ByteString.Lazy as B
 import qualified System.Directory as D
 import Control.Monad
 
+import qualified GitHub.Data as Github
+
 import Text.Regex.Posix
 
 import Debug.Trace
 
-type PackageDescriptions = Map.Map String Package.PackageDescription
+type AllPackageDescriptions = Map.Map String GithubAndHackagePackageDescription
 
-data AnnotatedPackageDescription = AnnotatedPackageDescription {
-    description :: Package.PackageDescription
+data GithubAndHackagePackageDescription = GithubAndHackagePackageDescription {
+    onHackage :: Bool,
+    packageName :: String,
+    maybeGithubName :: Maybe String,
+    maybePackageDescription :: Maybe Package.PackageDescription,
+    maybeGithubDescription :: Maybe Github.Repo
 }
+
+-- We want to show for each package how many github projects depend on it
+-- that are not also on hackage.
+-- So we want to have all github repos, then exclude the ones that we already have
+-- the cabal file for. Then get the cabal file for all the ones we don't have
+-- a cabal file for. Then for all the cabal files we get from github, we want
+-- to discard all the projects with cabal files that identify a project that already
+-- is on hackage. Then we want to discard all the projects that have less stars
+-- than a project that we already have the cabal file of
 
 main :: IO ()
 main = do
@@ -33,24 +48,32 @@ main = do
   return ()
   -- TODO annotate package descriptions with github info
 
-makeGithubProjectsMap :: [AE.Value] -> Map.Map String AE.Value
-makeGithubProjectsMap projects = foldl addProject Map.empty projects
-  where
-    addProject m p = Map.insert (getKey p) p m
-    getKey (AE.Object o) = toKey $ M.fromJust $ HMap.lookup "repoHtmlUrl" o
-    toKey (AE.String s) = show s
-
+readGithubProjects :: IO ([Github.Repo])
 readGithubProjects = do
   filenames <- liftM (filter (\ f -> (take 5 f) == "repos")) $ D.listDirectory $ "data/"
   files <- mapM B.readFile filenames
-  let parsedFiles = M.mapMaybe AE.decode' files
-  return $ concatMap (\(AE.Array v) -> V.toList v) parsedFiles
+  let parsedFiles = M.mapMaybe AE.decode' files :: [[Github.Repo]]
+  return $ concat parsedFiles
 
-filterOnCabal packages projects = M.mapMaybe isInPackages $ V.toList projects
+makeGithubProjectsMap :: [Github.Repo] -> Map.Map String Github.Repo
+makeGithubProjectsMap projects = foldl addProject Map.empty projects
   where
-    isInPackages :: AE.Value -> Maybe AE.Value
-    isInPackages p@(AE.Object o) = Just p
-    isInPackages x = trace ("Something was not an object: " ++ (show x)) $ undefined
+    addProject m p = maybeInsert (getKey p) p m
+    maybeInsert k p m = (\ p'' -> Map.insert k p'' m) $ case Map.lookup k m of
+      Just p' -> betterProject p p'
+      Nothing -> p
+    betterProject p p' = if starsP > starsP' then p else p'
+      where
+        starsP = Github.repoStargazersCount p
+        starsP' = Github.repoStargazersCount p'
+    getKey o = toKey $ Github.repoName o
+    toKey s = show s
+
+-- filterOnCabal packages projects = M.mapMaybe isInPackages $ V.toList projects
+--   where
+--     isInPackages :: Github.Repo -> Maybe AE.Value
+--     isInPackages p@(AE.Object o) = head $ M.catMaybes $ getGithubRepos o
+--     isInPackages x = trace ("Something was not an object: " ++ (show x)) $ undefined
     -- githubRepoParser :: Parser (Maybe String)
     -- githubRepoParser = do
     --   CMC.skipManyTill MP.anyChar $ do
@@ -61,5 +84,11 @@ filterOnCabal packages projects = M.mapMaybe isInPackages $ V.toList projects
     --   repo <- CMC.manyTill MP.anyChar (MP.noneOf ['/', ' ', '?'])
     --   return $ Just $ owner ++ "/" ++ repo
 
-    extractGithubRepo :: String -> String
-    extractGithubRepo url = url =~ ("github\\.com[:/][^/]+/[^/ ?]+" :: String)
+    extractGithubRepo :: String -> Maybe String
+    extractGithubRepo url = maybeResult matchUrl
+      where
+        maybeResult [] = Nothing
+        maybeResult (s:[]) = trace ("Got this hit: " ++ (show s)) $ Just $ head s
+        maybeResult xs = trace ("Got more than one result: " ++ (show xs)) undefined
+        matchUrl :: [[String]]
+        matchUrl = url =~ ("github\\.com[:/][^/]+/[^/ ?]+" :: String)
